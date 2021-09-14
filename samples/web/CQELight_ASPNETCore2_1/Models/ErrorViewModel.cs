396cb0bfb824090f393c114c5132c7f6fcbf23ca
Christophe Mommer <hybrid-technologies-solutions@outlook.com>
2021-09-14 20:48:56 +0200

    drop support for ASP.NET 2.1

--- samples/web/CQELight_ASPNETCore2_1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CQELight_ASPNETCore2_1.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
